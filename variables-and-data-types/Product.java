d29bc5a3a8d6226f163a27e286c92424fa6312b1
Rishiraj Tamuly <107537483+rishirajtamuly@users.noreply.github.com>
2024-01-04 15:16:01 +0530

    Create Product.java

---
+++ variables-and-data-types/Product.java
@@ -0,0 +1,15 @@
+// Calculate the product of two numbers
+
+import java.util.*;
+
+public class Product {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.println("Product of " + a + " and " + b + " is : " + product);
+    }
+}
