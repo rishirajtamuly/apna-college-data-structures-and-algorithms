7ae777b1897ce040027abfd2aba331c899d530dc
Rishiraj Tamuly <107537483+rishirajtamuly@users.noreply.github.com>
2024-01-05 23:30:26 +0530

    Create Calculator.java

---
+++ conditional_statements/Calculator.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a : ");
+        int a = sc.nextInt();
+        System.out.print("Enter b : ");
+        int b = sc.nextInt();
+        System.out.print("Enter operator : ");
+        char operator = sc.next().charAt(0);
+        sc.close();
+
+        switch (operator) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            case '%':
+                System.out.println(a % b);
+                break;
+            default:
+                System.out.println("Wrong Operator");
+        }
+    }
+}
