b866609285b7348b166b25abfb6fce2dfac2a35f
Rishiraj Tamuly <107537483+rishirajtamuly@users.noreply.github.com>
2024-01-07 22:18:51 +0530

    Create SumEvenOdd.java

---
+++ loops/SumEvenOdd.java
@@ -0,0 +1,33 @@
+/*  Write a program that reads a set of integers, and then prints the sum of the
+even and odd integers. */
+
+import java.util.*;
+
+public class SumEvenOdd {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+        int choice;
+        int evenSum = 0;
+        int oddSum = 0;
+
+        do {
+            System.out.print("Enter a number : ");
+            number = sc.nextInt();
+
+            if (number % 2 == 0) {
+                evenSum += number;
+            } else {
+                oddSum += number;
+            }
+
+            System.out.print("Do you want to continue? Press 1 for yes or 0 for no : ");
+            choice = sc.nextInt();
+        } while (choice == 1);
+
+        sc.close();
+        System.out.println("Sum of even numbers : " + evenSum);
+        System.out.println("Sum of odd numbers : " + oddSum);
+    }
+}
