27d0f6a321c66e3b15ffd7e91d16acfa11b96883
Rishiraj Tamuly <107537483+rishirajtamuly@users.noreply.github.com>
2024-01-08 22:20:24 +0530

    Create Sum.java

---
+++ functions-and-methods/Sum.java
@@ -0,0 +1,22 @@
+// Calculate sum of two numbers
+
+import java.util.*;
+
+public class Sum {
+
+    public static int calculateSum(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum;
+    } 
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1st number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter 2nd number : ");
+        int b = sc.nextInt();
+        sc.close();
+        int sum = calculateSum(a, b);
+        System.out.println("Sum is : " + sum);
+    }
+}
