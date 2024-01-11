ff722ddc071b92806372fdd04c2415e4b08960f4
Rishiraj Tamuly <107537483+rishirajtamuly@users.noreply.github.com>
2024-01-11 09:46:59 +0530

    Create PalindromeNumber.java

---
+++ functions-and-methods/PalindromeNumber.java
@@ -0,0 +1,40 @@
+/*  Write a Java program to check if a number is a palindrome in Java? ( 121 is a
+palindrome, 321 is not)
+A number is called a palindrome if the number is equal to the reverse of a number e.g., 121 is a
+palindrome because the reverse of 121 is 121 itself. On the other hand, 321 is not a
+palindrome because the reverse of 321 is 123, which is not equal to 321.    */
+ 
+import java.util.*;
+
+public class PalindromeNumber {
+    
+    public static boolean isPalindrome(int number) {
+        int palindrome = number;
+        int reverse = 0;
+
+        while (palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+
+        if (number == reverse) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        sc.close();
+        
+        if (isPalindrome(num)) {
+            System.out.println(num + " is palindrome");
+        } else {
+            System.out.println(num + " is not palindrome");
+        }
+    }
+}
